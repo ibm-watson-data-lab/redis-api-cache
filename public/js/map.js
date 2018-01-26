d2cd9285571c9db2cb46645b83d27e80b2757afb
Abdullah Alger <abdullah.alger@ibm.com>
2018-01-26 09:20:30 -0800

    reorganized code

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,5 @@
 // Mapbox access token.
-mapboxgl.accessToken = "pk.eyJ1IjoiYWFsZ2VyIiwiYSI6ImNqMzB2OGJlbjAwMW8zM2s4cWVsY3IybWIifQ.9qDiHbV9N5ezaQ8czC9gew";
+mapboxgl.accessToken = "";
 
 // Initialize Mapbox and place the center of the map on the United States.
 const map = new mapboxgl.Map({
