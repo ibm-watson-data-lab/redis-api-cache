58792cfc2c4847683c42a41b3fa5a77484fed1e4
Abdullah Alger <abdullah.alger@ibm.com>
2018-01-26 09:19:15 -0800

    reorganized code

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const redis = require("redis");
-const morgan = require("morgan");
 const url = require("url");
 const fetch = require("node-fetch");
 
-app.use(express.static(__dirname));
-
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
+app.use(express.static(__dirname + "/public"));
 
 const apiKey = process.env.API_KEY;
-
-let connectionString = process.env.COMPOSE_REDIS_URL;
+const connectionString = process.env.COMPOSE_REDIS_URL;
 
 if (connectionString === undefined) {
   console.error("Please set the COMPOSE_REDIS_URL environment variable");
@@ -35,30 +26,28 @@ if (connectionString.startsWith("rediss://")) {
 
 app.get('/api/colleges', (req, res) => {
   let query = req.query.college;
-  let college = `college/${query}`.trim();
+  let college = `college/${query}`.trim().toLowerCase();
   client.get(college, (err, data) => {
     if (err) throw err;
     if (data !== null) {
       res.send(data);
     } else {
-      console.log("Cache missed for " + query);
       fetch(
         "https://api.data.gov/ed/collegescorecard/v1/schools?api_key=" +
           apiKey +
           "&school.name=" +
           query +
-          "&fields=school.name,id,location.lon,location.lat&per_page=100"
+          "&fields=school.name,id,location.lon,location.lat&_per_page=100"
       )
         .then(res => res.json())
         .then(json => {
           console.log(json)
-          client.set(college, JSON.stringify(json));
-          //client.setex(college, 300, JSON.stringify(json));
+          client.setex(college, 300, JSON.stringify(json));
           res.send(json);
         })
         .catch(err => {
           console.error(err);
-          resp.send(500);
+          resp.send(202);
         });
     }
 
